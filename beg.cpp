9f4b89db6278a3dab2c8b6a018d4ee50477b9188
kirmada150905 <74258997+kirmada150905@users.noreply.github.com>
2024-11-19 00:04:00 +0530

    macros

---
+++ beg.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+#define get(A, N) for(int i = 0; i < N; i++){int temporary_for_input; cin>>temporary_for_input; A.push_back(temporary_for_input);}
+#define sort_ass(A) sort(A.begin(), A.end())
+#define sort_des(A) sort(A.begin(), A.end(), [](int a, int b){return a > b;})
+#define print(A) {for(int x: A)cout<<x<<" ";}cout<<endl;
+
+void solve(){
+
+}
+
+int main(){
+    solve();
+}
